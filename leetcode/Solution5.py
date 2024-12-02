54dce799b3782a1a89836c10d543f698ceaa4a23
Chakravarthi Ponmudi <chakravarhtiponmudi@gmail.com>
2024-12-02 07:31:43 +0530

    ..

--- leetcode/Solution5.py
+++ leetcode/Solution5.py
@@ -6,4 +6,27 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        left = heapq
+        minHeap = []
+        maxHeap = []
+        
+        num1iter = iter(nums1)
+        num2iter = iter(nums2)
+        
+        
+
+class MaxHeap():
+   
+    def heahpush(self, li, item):
+        heapq.heappush(li,-item)
+    
+    def heappop(self, li):
+        -heapq.heappop(li)
+    
+    def heappushpop(li,item):
+        a = heapq.heappushpop(li,-item)
+        return -a
+    
+    def heapreplace(self, li, item):
+        a = heapq.heapreplace(li,-item)
+        return -a;
+                
